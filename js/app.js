a30fba1bc53e05b04576f47d371702ee83bd8203
@parou.char <curlycode@outlook.com>
2024-10-23 15:10:37 +0300

    add js functionality

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let containerTop = document.getElementById('topId');
+//add fuctionality to hide the bar if the close button is clicked.
+document.addEventListener('click',function(){
+    containerTop.classList.add('close-top');
+}
+);
